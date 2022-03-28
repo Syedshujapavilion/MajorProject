f40db9ea2007c421c6efb2dd740cfa7f5f9d9e7e
Shuja <shujapavilion150@gmail.com>
2022-03-28 16:03:33 +0530

    Online compiler chalra boss

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
 
 app = express();
 
@@ -7,29 +8,39 @@ app.set('template engine', 'html');
 app.set('views', path.join(__dirname, 'templates'));
 app.use('/static', express.static('static'))
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.status(200).render('home.pug')
 })
 
-app.get('/register', (req, res)=>{
+app.get('/register', (req, res) => {
     res.status(200).render('register.pug')
 })
 
-app.get('/login', (req, res)=>{
+app.get('/login', (req, res) => {
     res.status(200).render('login.pug')
 })
 
-app.get('/logout', (req, res)=>{
+app.get('/logout', (req, res) => {
     res.status(200).render('logout.pug')
 })
 
-app.get('/blog-home', (req, res)=>{
+app.get('/blog-home', (req, res) => {
     blogs = [
         {
             'title': "Hello1",
             'content': "World1",
             'author': "user1"
-        }, 
+        },
+        {
+            'title': "Hello2",
+            'content': "World2",
+            'author': "user2"
+        },
+        {
+            'title': "Hello1",
+            'content': "World1",
+            'author': "user1"
+        },
         {
             'title': "Hello2",
             'content': "World2",
@@ -39,8 +50,56 @@ app.get('/blog-home', (req, res)=>{
     res.status(200).render('blog-home.pug', {})
 })
 
+app.get('/blog/submit', (req, res) => {
+    res.status(200).render('blog-submit.pug', {})
+})
+
+// APNA NAYA CODE 
+const cors = require("cors");
+const Axios = require("axios");
+app.use(cors());
+app.use(express.json());
+
+app.use( bodyParser.json() )
+app.use(express.urlencoded());
+
+app.post("/compile", (req, res) => {
+
+    let code = req.body.codeBeta
+    let language = req.body.language
+    let input = req.body.input
+
+    let data = ({
+        "code": code,
+        "language": language,
+        "input": input
+    });
+
+    let config = {
+        method: 'post',
+        url: 'https://codexweb.netlify.app/.netlify/functions/enforceCode',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: data
+    };
+    
+    Axios(config)
+        .then((response) => {
+            // res.send(response.data)
+            res.status(200).render('compile.pug', {'output':response.data})
+            console.log(response.data)
+        }).catch((error) => {
+            console.log(error);
+        });
+})
+
+app.get('/compile', (req, res) => {
+    res.status(200).render('compile.pug', {output : ""})
+})
+
 port = 80
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+})
